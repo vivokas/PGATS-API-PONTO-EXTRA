c65455619934eef7194cf1d7087e6f02694c39af
Vivokas <vivianevinici@gmail.com>
2025-08-09 17:38:09 -0300

    subindo alterações

--- controller/transferController.js
+++ controller/transferController.js
@@ -8,10 +8,12 @@ exports.transfer = (req, res) => {
   if (!from || !to || typeof value !== 'number') {
     return res.status(400).json({ error: 'Remetente, destinatário e valor são obrigatórios.' });
   }
+
   const result = transferService.transfer({ from, to, value });
   if (result.error) {
     return res.status(400).json({ error: result.error });
   }
+  
   res.status(201).json(result.transfer);
 };
 
